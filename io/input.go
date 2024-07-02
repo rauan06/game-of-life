acb9a878dd939191c5c08175462c33ed1c099afc
Rauan <138851982+rauan06@users.noreply.github.com>
2024-07-02 20:53:46 +0500

    create input handler

--- io/input.go
+++ io/input.go
@@ -1,9 +1,12 @@
 package io
 
 import (
+	"bufio"
 	"fmt"
 	"game/assets"
-	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func MaintainInput(flagsMap map[string]interface{}) [][]int {
@@ -12,21 +15,85 @@ func MaintainInput(flagsMap map[string]interface{}) [][]int {
 	}
 
 	if flagsMap["random"] == false {
-		temp, err := InputHandler()
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		return temp
+		return InputHandler()
 	}
 	return assets.RandomGrid()
 }
 
-func InputHandler() ([][]int, error) {
-	var w, h int
-	fmt.Print("Enter the size of the grid (width x height): ")
-	fmt.Scanf("%d %d", w, h)
+func InputHandler() [][]int {
+	var h, w int
 
-	return [][]int{}, nil
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		var err error
+		fmt.Print("Enter the size of the grid (height x width): ")
+
+		firstLine, _ := reader.ReadString('\n')
+		firstLine = strings.TrimSpace(firstLine)
+
+		firstLineArgs := strings.SplitN(firstLine, " ", 2)
+
+		h, err = strconv.Atoi(firstLineArgs[0])
+
+		if err != nil {
+			fmt.Println("invalid input, please try again")
+			continue
+		}
+
+		w, err = strconv.Atoi(firstLineArgs[1])
+
+		if err != nil {
+			fmt.Println("invalid input, please try again")
+			continue
+		}
+
+		break
+	}
+
+	grid := make([][]int, h)
+	for r := range grid {
+		grid[r] = make([]int, w)
+	}
+
+	return readGrid(h, w)
+}
+
+func readGrid(h, w int) [][]int {
+	reader := bufio.NewReader(os.Stdin)
+	grid := make([][]int, h)
+
+	for r := 0; r < h; r++ {
+		grid[r] = make([]int, w)
+	}
+
+	counter := 1
+	for {
+		if counter > h {
+			break
+		}
+		fmt.Printf("Enter row %d: ", counter)
+		line, _ := reader.ReadString('\n')
+		line = strings.TrimSpace(line)
+
+		if len([]rune(line)) == w {
+			arr := []int{}
+
+			for _, r := range line {
+				if r == '.' {
+					arr = append(arr, 0)
+				} else {
+					arr = append(arr, 1)
+				}
+			}
+
+			grid[counter-1] = arr
+			counter++
+		} else {
+			fmt.Println("Invalid line length, please try again")
+			continue
+		}
+	}
+
+	return grid
 }
